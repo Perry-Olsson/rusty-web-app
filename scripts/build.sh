e7d2e42b3db93cc131ae478f6ccbfa471a78f622
Perry Olsson <olsson.perry@gmail.com>
2025-11-29 14:45:20 -0800

    Removing old dev scripts

--- scripts/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-script_dir="$(dirname "$(readlink -f "$0")")"
-
-source $script_dir/metadata.sh
-
-docker exec -t $APP_NAME cargo build --manifest-path /$APP_NAME/Cargo.toml
--- scripts/metadata.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-APP_NAME="reddit-clone"
