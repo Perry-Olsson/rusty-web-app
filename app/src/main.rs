62c4994a375e12cc9a336e3c22873552f2c09460
Perry Olsson <olsson.perry@gmail.com>
2025-11-30 14:12:26 -0800

    post endpoints

--- app/src/main.rs
+++ app/src/main.rs
@@ -1,10 +1,35 @@
-use actix_web::{get, App, HttpResponse, HttpServer, Responder};
+use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use serde::{Deserialize, Serialize};
 
 const PORT: u16 = 8080;
+
+#[derive(Serialize, Deserialize)]
+struct Post {
+    title: String,
+    content: String,
+}
 
 #[get("/")]
 async fn hello() -> impl Responder {
     HttpResponse::Ok().body("hello world\n")
+}
+
+#[get("/post")]
+async fn get_post() -> impl Responder {
+    let post = _get_post();
+    web::Json(post)
+}
+
+fn _get_post() -> Post {
+    Post {
+        title: "My First Post".to_string(),
+        content: "This is some dummy content for the post.".to_string(),
+    }
+}
+
+#[post("/post")]
+async fn create_post() -> impl Responder {
+    HttpResponse::Ok().body("POST /post\n")
 }
 
 #[actix_web::main]
@@ -12,6 +37,8 @@ async fn main() -> std::io::Result<()> {
     let server = HttpServer::new(|| {
         App::new()
             .service(hello)
+            .service(get_post)
+            .service(create_post)
     })
     .bind(("0.0.0.0", PORT))?
     .run();
