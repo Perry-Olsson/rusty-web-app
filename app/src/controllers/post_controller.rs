4a46afb4425994648bde7c96644ba5be46024213
Perry Olsson <olsson.perry@gmail.com>
2025-11-30 16:34:20 -0800

    Post service

--- app/src/controllers/post_controller.rs
+++ app/src/controllers/post_controller.rs
@@ -1,16 +1,16 @@
 use actix_web::{get, post, web, HttpResponse, Responder};
 
-use crate::service::post_service::{GetPostQuery, NewPost, PostService};
+use crate::{service::post_service::{GetPostQuery, NewPost}, Services};
 
 #[get("/post")]
-pub async fn get_post(query: web::Query<GetPostQuery>) -> impl Responder {
-    let post = PostService::new().get_post(&query);
+pub async fn get_post(services: web::Data<Services>, query: web::Query<GetPostQuery>) -> impl Responder {
+    let post = services.post_service.get_post(&query);
     web::Json(post)
 }
 
 #[post("/post")]
-pub async fn create_post(post: web::Json<NewPost>) -> impl Responder {
-    match PostService::new().create_post(post.into_inner()) {
+pub async fn create_post(services: web::Data<Services>, post: web::Json<NewPost>) -> impl Responder {
+    match services.post_service.create_post(post.into_inner()) {
         Ok(post) => HttpResponse::Ok().json(post),
         Err(err) => HttpResponse::BadRequest().body(err),
     }
--- app/src/lib.rs
+++ app/src/lib.rs
@@ -2,9 +2,11 @@ mod controllers;
 mod models;
 mod service;
 
-use actix_web::{get, App, HttpResponse, HttpServer, Responder};
+use actix_web::{get, web, App, HttpResponse, HttpServer, Responder};
 
 use controllers::post_controller::{create_post, get_post};
+
+use crate::service::post_service::PostService;
 
 const PORT: u16 = 8080;
 
@@ -13,10 +15,16 @@ async fn hello() -> impl Responder {
     HttpResponse::Ok().body("hello world\n")
 }
 
+pub struct Services {
+    post_service: PostService
+}
 
 pub async fn run() -> std::io::Result<()> {
     let server = HttpServer::new(|| {
         App::new()
+            .app_data(web::Data::new(Services {
+                post_service: PostService::new()
+            }))
             .service(hello)
             .service(get_post)
             .service(create_post)
